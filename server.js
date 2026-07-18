bce7111bcb5c7e1b623bdd2bbf7bbcb0d6759581
ardin14 <2025bit245@std.must.ac.ug>
2026-07-18 23:02:56 +0300

    git commit -m "fix: add CORS headers for MikroTik-served portal"

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const cors = require("cors");
 const path = require("path");
 const { init } = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("trust proxy", 1);
